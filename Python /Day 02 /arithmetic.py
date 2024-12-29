9df22422cf654675ee831c2e378c02466ed29326
Diyabiswa <bdiya8157@gmail.com>
2024-12-29 22:22:19 +0530

    Arithmetic.py

---
+++ Python /Day 02 /arithmetic.py
@@ -0,0 +1,15 @@
+# Arithmetic operations
+a, b = 10, 3
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+modulus = a % b
+exponentiation = a ** b
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Modulus: {modulus}")
+print(f"Exponentiation: {exponentiation}")
