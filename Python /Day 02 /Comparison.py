f8d33fb205b2dbf9e9a0f7c2bd31263d720d9d17
Diyabiswa <bdiya8157@gmail.com>
2024-12-29 22:26:14 +0530

    Comparison.py

---
+++ Python /Day 02 /Comparison.py
@@ -0,0 +1,11 @@
+# Comparison operations
+x, y = 15, 20
+greater_than = x > y
+less_than = x < y
+equal_to = x == y
+not_equal = x != y
+
+print(f"Greater than: {greater_than}")
+print(f"Less than: {less_than}")
+print(f"Equal to: {equal_to}")
+print(f"Not equal: {not_equal}")
