ce3c69aeccee66072da358120dba4d74936640b5
Diyabiswa <bdiya8157@gmail.com>
2024-12-29 22:35:24 +0530

    If_else.py

---
+++ Python /Day 03/If_else.py
@@ -0,0 +1,12 @@
+# If-else
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
+
+"""
+OUTPUT
+  Enter a number: 6
+  6 is even.
+"""
