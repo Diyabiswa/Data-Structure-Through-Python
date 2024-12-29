bbba59a297c054c24a971da99a4092f09751b354
Diyabiswa <bdiya8157@gmail.com>
2024-12-29 22:29:19 +0530

    Logical.py

---
+++ Python /Day 02 /Logical.py
@@ -0,0 +1,16 @@
+# Logical operations
+p, q = True, False
+and_operation = p and q
+or_operation = p or q
+not_operation = not p
+
+print(f"AND operation: {and_operation}")
+print(f"OR operation: {or_operation}")
+print(f"NOT operation: {not_operation}")
+
+"""
+OUTPUT
+  AND operation: False
+  OR operation: True
+  NOT operation: False
+"""
