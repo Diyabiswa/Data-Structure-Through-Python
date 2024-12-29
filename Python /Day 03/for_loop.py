fff5c664fb363726b9c15be937ca2b8fd716135d
Diyabiswa <bdiya8157@gmail.com>
2024-12-29 22:37:40 +0530

    for_loop.py

---
+++ Python /Day 03/for_loop.py
@@ -0,0 +1,13 @@
+# For loop
+n = 5
+for i in range(1, n + 1):
+    print(f"Square of {i}: {i**2}")
+
+"""
+OUTPUT
+  Square of 1: 1
+  Square of 2: 4
+  Square of 3: 9
+  Square of 4: 16
+  Square of 5: 25
+"""
