d8d3bcbf4747f1ce690b6c4afdc40a911c267095
Diyabiswa <bdiya8157@gmail.com>
2025-02-17 19:04:19 +0530

    example.py
    Example of if_else and for loop

---
+++ Python /Day 03/example.py
@@ -0,0 +1,22 @@
+# List of numbers
+numbers = [10, 21, 32, 43, 54, 65, 76, 87, 98]
+
+# Loop through each number in the list
+for num in numbers:
+    # Check if the number is even or odd using if-else
+    if num % 2 == 0:
+        print(f"{num} is Even")
+    else:
+        print(f"{num} is Odd")
+
+"""
+10 is Even
+21 is Odd
+32 is Even
+43 is Odd
+54 is Even
+65 is Odd
+76 is Even
+87 is Odd
+98 is Even
+"""
